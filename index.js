fed38a4fd913389b7018f594f04e2f9f5cd0d4d6
000tiago <tiagorodriguesnutri@gmail.com>
2025-02-06 10:40:19 -0300

    Ajuste para deixar responsivo

--- index.js
+++
@@ -1,9 +0,0 @@
-const logoImage = document.querySelector('.logo-image');
-
-logoImage.addEventListener('mouseenter', () => {
-    logoImage.src = './src/assets/icons/logo-spotifyecho.png'; // Altere para o caminho da nova imagem
-});
-
-logoImage.addEventListener('mouseleave', () => {
-    logoImage.src = './src/assets/icons/logo-spotify.png'; // Volta para a imagem original
-});
